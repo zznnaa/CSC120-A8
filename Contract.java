d354b8349a79fd00e8bb982637113d65ab596dc5
R. Jordan Crouser <jcrouser@smith.edu>
2022-11-08 22:17:10 -0600

    Add files via upload

---
+++ Contract.java
@@ -0,0 +1,14 @@
+public interface Contract {
+
+    void grab(String item);
+    String drop(String item);
+    void examine(String item);
+    void use(String item);
+    boolean walk(String direction);
+    boolean fly(int x, int y);
+    Number shrink();
+    Number grow();
+    void rest();
+    void undo();
+
+}
